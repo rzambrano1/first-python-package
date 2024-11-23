b28e1cf8ac207d5e63e998124a5b0292fba3ca32
rzambrano1 <rzambrano@gmail.com>
2024-11-23 13:05:56 -0500

    end_ch_5

--- src/imppkg/harmony.py
+++ src/imppkg/harmony.py
@@ -3,5 +3,20 @@ from imppkg.harmonic_mean import harmonic_mean
 from termcolor import cprint
 
 def main():
-    nums = [float(arg) for arg in sys.argv[1:]]
-    cprint(harmonic_mean(nums), 'red', 'on_cyan', attrs=['bold'])
+
+    result = 0.0 # The result will be zero unless succesfully calculated
+
+    try:
+        nums = [float(arg) for arg in sys.argv[1:]]
+    
+    except ValueError:
+        # If input cannot be converted to float, proceed as if there was no input
+        nums = []
+    
+    try:
+        result = harmonic_mean(nums)
+    except ZeroDivisionError:
+        # If there is no input or if the input is zero, rpoceed with the default result
+        pass
+    
+    cprint(result, 'red', 'on_cyan', attrs=['bold'])
--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -1,2 +1,61 @@
+import sys
+from termcolor import colored
+import pytest
+
+from imppkg.harmonic_mean import harmonic_mean
+from imppkg.harmony import main
+
 def test_always_passes():
     assert True
+
+@pytest.mark.parametrize(
+    "input_one, input_two, input_three, expected",
+    [
+        ("1", "4", "4", 2.0),
+        (0, 0, 0, 0.0),
+        ("a", "b", "c", 0.0)
+    ]
+)
+def test_harmony_parametrized(input_one, input_two, input_three, expected, monkeypatch, capsys):
+    inputs = [input_one, input_two, input_three]
+    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+    main()
+
+    expected_value = expected
+    assert capsys.readouterr().out.strip() == colored(
+        expected_value,
+        "red",
+        "on_cyan",
+        attrs = ["bold"]
+    )
+
+
+# def test_harmony_happy_path(monkeypatch, capsys):
+#     inputs = ["1", "4", "4"]
+#     monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+#     main()
+
+#     expected_value = 2.0
+#     assert capsys.readouterr().out.strip() == colored(
+#         expected_value,
+#         "red",
+#         "on_cyan",
+#         attrs = ["bold"]
+#     )
+
+# def test_harmony_non_num_imput(monkeypatch, capsys):
+#     inputs = ["a", "b", "c"]
+
+#     monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+#     main()
+
+#     expected_value = 0.0
+#     assert capsys.readouterr().out.strip() == colored(
+#         expected_value,
+#         "red",
+#         "on_cyan",
+#         attrs = ["bold"]
+#     )
