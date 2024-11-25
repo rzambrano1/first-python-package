144d7957d1b9de40caa7ed02e5ee80f90cbf41ec
rzambrano1 <rzambrano@gmail.com>
2024-11-24 19:25:07 -0500

    figuring_out_pr

--- src/imppkg/hello.py
+++ src/imppkg/hello.py
@@ -1,1 +1,8 @@
 # Hello, this is a comment to test the test_branch 
+
+# To create the pull request from VS Code I had to run the following commands from 
+# the terminal:
+
+#> git remote remove origin
+#> git remote add origin https://github.com/rzambrano1/first-python-package.git
+
