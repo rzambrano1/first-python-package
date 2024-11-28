d39a309a860895cd69a015eea832a2693b3106e5
rzambrano1 <rzambrano@gmail.com>
2024-11-28 18:34:29 -0500

    errors_from_format_fixed

--- src/imppkg/hello.py
+++ src/imppkg/hello.py
@@ -1,8 +1,7 @@
-# Hello, this is a comment to test the test_branch 
+# Hello, this is a comment to test the test_branch
 
-# To create the pull request from VS Code I had to run the following commands from 
+# To create the pull request from VS Code I had to run the following commands from
 # the terminal:
 
-#> git remote remove origin
-#> git remote add origin https://github.com/rzambrano1/first-python-package.git
-
+# > git remote remove origin
+# > git remote add origin https://github.com/rzambrano1/first-python-package.git
